372f24f49ecb8b8bcce6ca8efc26ad35eb523ecd
KunalBhansali31 <kunalbhansali425@yahoo.com>
2023-03-16 21:59:10 -0400

    Create MyCharRunner.java

---
+++ CoreJavaProjects/src/basic_Java_Codes/MyCharRunner.java
@@ -0,0 +1,14 @@
+package basic_Java_Codes;
+
+public class MyCharRunner {
+
+	public static void main(String[] args) {
+		MyChar myChar = new MyChar('c');
+		System.out.println(myChar.isVowel());
+		System.out.println(myChar.isDigit());
+		System.out.println(myChar.isAlphabet());
+		MyChar.printLowerCaseAlphabets();
+		MyChar.printUpperCaseAlphabets();
+	}
+
+}
