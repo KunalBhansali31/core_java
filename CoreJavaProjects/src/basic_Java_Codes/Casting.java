1462be8076ca714ccf874eb1125cbed8eb8745d6
KunalBhansali31 <kunalbhansali425@yahoo.com>
2023-03-16 21:58:10 -0400

    Create Casting.java

---
+++ CoreJavaProjects/src/basic_Java_Codes/Casting.java
@@ -0,0 +1,15 @@
+package basic_Java_Codes;
+
+public class Casting {
+	public static void main(String[] args) {
+int i= 10000;
+long l= 50000000l;
+//i = l;
+i = (int) l; // casting a vlaue which is of larger type to smaller is called casting
+System.out.println(i);
+}
+
+}
+// octal 0-7
+//hexadecimal 0-9 and A to F
+//int fifteen = 0XF
