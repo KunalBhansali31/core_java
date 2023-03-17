1620958eb625f32f410eb8ae41efa886a98117df
KunalBhansali31 <kunalbhansali425@yahoo.com>
2023-03-16 21:59:04 -0400

    Create multiplicationTableRunner2.java

---
+++ CoreJavaProjects/src/basic_Java_Codes/multiplicationTableRunner2.java
@@ -0,0 +1,17 @@
+package basic_Java_Codes;
+
+public class multiplicationTableRunner2 {
+
+	public static void main(String[] args) {
+		// Perform Multiplication table
+		multiplicationTable2 m2 = new multiplicationTable2();
+		// Perform Multiplication table using no argument
+		m2.print();
+		// Perform Multiplication table using single argument
+		m2.print(6);
+		// Perform Multiplication table using multiple argument
+		m2.print(6, 11, 20);
+
+	}
+
+}
